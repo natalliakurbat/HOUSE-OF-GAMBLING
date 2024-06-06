cf80d5bf4c89cd84757cacfab27d48225bfa34dc
natalliakurbat <146668174+natalliakurbat@users.noreply.github.com>
2024-06-06 18:56:17 +0300

    adaptive layout up to 960px

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@ let buttonLang = document.querySelector('.language');
 let textLang = document.querySelector('.textLang');
 
 buttonLang.addEventListener('click', () => {
-    /* console.log(buttonLang.textContent);*/
     if (textLang.textContent == "en") {
         textLang.textContent = "ru";
     } else {
